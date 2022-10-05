17b8dfc1490a3c0df568236c04b7f335aa726b16
rugwizangoga <pacific.r.iradukunda@gmail.com>
2022-10-05 15:25:37 +0100

    created a todo list from the blueprint

--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@ import './style.css';
 
 const lst = document.querySelector('.list');
 
-const performers = [
+let Tasks = [
   {
     description: 'Wash the dishes',
     completed: true,
+    index: 1
+  },
+  {
+    description: 'complete the To Do list project',
+    completed: false,
     index: 2
   },
   {
     description: 'physical execise',
     completed: false,
     index: 0
-  },
-  {
-    description: 'complete the To Do list project',
-    completed: false,
-    index: 1
   },
 ];
 
@@ -31,19 +31,19 @@ lst.innerHTML = `
                       Add to your list...
                       <i class="fa-thin fa-arrow-turn-down-left"></i>
                   </li>
-                 
-                  <li class="listitem">
-                      <div><input type="checkbox">&nbsp;&nbsp; complete the To Do list project</div>
-                      <i class="fa-solid fa-ellipsis-vertical"></i>
                   </li>
                   <li class="listitem last"><button type="button" class="complete">clear all completed</button></li>
                 `
 
-const litem = document.createElement('li');
 
-litem.innerHTML = `<div><input type="checkbox">&nbsp;&nbsp; Wash the dishes</div>
-                    <i class="fa-solid fa-ellipsis-vertical"></i>
-                  `
-litem.classList.add('listitem');
-
-lst.insertBefore(litem, lst.children[2]);
+Tasks=_.sortBy(Tasks, [function(o) { return o.index; }]);
+let ind = 2
+Tasks.forEach(task =>{
+  const litem = document.createElement('li');
+  litem.innerHTML = `<div><input type="checkbox">&nbsp;&nbsp; ${task.description}</div>
+                      <i class="fa-solid fa-ellipsis-vertical"></i>
+                    `
+  litem.classList.add('listitem');
+  lst.insertBefore(litem, lst.children[ind]);
+  ind++;
+});
